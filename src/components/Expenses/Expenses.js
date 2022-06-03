e8a6624576b65d9a6de85827f8b54430b62a0745
irinakalman <kalman0292@gmail.com>
2022-06-03 03:59:33 +0300

    2nd project in React

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+
+
+
+function Expenses(props) {
+    // let expenseList = [];
+    // console.log(props.expenses);
+    // props.expenses.forEach(expense => {
+        
+    //   expenseList.push(<ExpenseItem
+    //     title={expense.title}
+    //     amount={expense.amount}
+    //     date={expense.date}
+    //   ></ExpenseItem>)  
+    // });
+  return (
+    <Card className="expenses">
+        {/* {expenseList} */}
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+}
+
+export default Expenses;
